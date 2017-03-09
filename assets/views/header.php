9e0ff5c5e7747747f94c34d3ef54c085ee69911f
camisLuisa <clfl@ecomp.poli.br>
2017-03-09 15:52:40 -0300

    fixed bar

--- assets/views/header.php
+++ assets/views/header.php
@@ -1,4 +1,3 @@
-<nav class="navbar navbar-default navbar-static-top">
 <nav class="navbar navbar-default navbar-fixed-top"><!-- queria por o navbar fixed mas aconteceu um problema semelhante a algo que alyson uma vez falou... perguntar a ele como resolver -->
 	<div class="container-fluid">
 	<div class=navbar-header">
@@ -8,7 +7,7 @@
 			<h1 class="navbar-brand" href="#">{{title}}</h1>
 		</div>
 		<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-			<a href="http://localhost/Cres_pass/" title="crescendo e passando"><img src="http://www.mibebeyyo.com/images/home/seguridad-objetos-cocina-1293.jpg" class="img-circle" width="200" height="200"></a>
+			<a href="http://localhost/Cres_pass/" title="crescendo e passando"><img src="http://www.mibebeyyo.com/images/home/seguridad-objetos-cocina-1293.jpg" class="img-circle" width="100" height="100"></a>
 		</div>
 		
 		<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
@@ -40,5 +39,4 @@
 	<li><a href="#">Lojinhas</a></li>
 </ul>
 	</div>
-</nav>
 </nav>
