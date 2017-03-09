3bd39bf5f6dd5ad5a71673d3ddb5e28e54d95b63
camisLuisa <clfl@ecomp.poli.br>
2017-03-09 15:13:23 -0300

    alteracoes header e footer

--- assets/views/header.php
+++ assets/views/header.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-default "><!-- queria por o navbar fixed mas aconteceu um problema semelhante a algo que alyson uma vez falou... perguntar a ele como resolver -->
+<nav class="navbar navbar-default navbar-fixed-top"><!-- queria por o navbar fixed mas aconteceu um problema semelhante a algo que alyson uma vez falou... perguntar a ele como resolver -->
 	<div class="container-fluid">
 	<div class=navbar-header">
 		<div ng-controller="HeaderController">
@@ -6,18 +6,34 @@
 		<div class="row">
 			<h1 class="navbar-brand" href="#">{{title}}</h1>
 		</div>
+		<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+			<a href="http://localhost/Cres_pass/" title="crescendo e passando"><img src="http://www.mibebeyyo.com/images/home/seguridad-objetos-cocina-1293.jpg" class="img-circle" width="200" height="200"></a>
+		</div>
+		
+		<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+		 	<form class="navbar-form navbar-right">
+		 	
+		 		<div class="form-group">
+		 			<input type="text class="form-control" placeholder="Digite o nome do produto, marca ou lojinha">
+		 		</div>
+		 		<button type="submit" class="btn btn-default">Buscar</button>
+		 </div>
 
-		<a href="http://localhost/Cres_pass/" title="crescendo e passando"><img src="http://www.mibebeyyo.com/images/home/seguridad-objetos-cocina-1293.jpg" class="img-circle" width="200" height="200"></a>
-		 <form class="navbar-form navbar-right">
-		 	<div class="form-group">
-		 		<input type="text class="form-control" placeholder="Digite o nome do produto, marca ou lojinha">
-		 	</div>
-		 	<button type="submit" class="btn btn-default">Buscar</button>
-		 	</form>
+		 <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+		 	<a href="#"><span></span> Login</a></form>
+			<a href="#"><span></span> Inscrever-se</a></form>
+		</div> 	
 	</div>
 	</div>
+	
 <ul class="nav navbar-nav">
-	<li><a href="#">Home</a></li>
+	<li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Page 1</b> <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="#">Page 1-1</a></li>
+          <li><a href="#">Page 1-2</a></li>
+          <li><a href="#">Page 1-3</a></li>
+        </ul>
+      </li>
 	<li><a href="#">Produtos</a></li>
 	<li><a href="#">Marcas</a></li>
 	<li><a href="#">Lojinhas</a></li>
