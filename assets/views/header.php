d4105204fa9a1ab0058011f2dc19048d540982da
FederalCircle <alyson.maia@gmail.com>
2017-03-13 19:59:23 -0300

    login modal w/ bug

--- assets/views/header.php
+++ assets/views/header.php
@@ -20,7 +20,7 @@
 						 </div>
 
 						 <div class="col-md-4">
-						 		<button class="btn btn-primary" ng-click="go('/login')">LOGIN</button>
+						 		<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#loginModal">LOGIN</button>
 
 						 		<button class="btn btn-primary" ng-click="go('/cadastro')">CADASTRO</button>
 						</div> 
@@ -54,4 +54,37 @@
 			</nav>
 		</div>
 	</div>
-</div>
+
+	<!-- LOGIN MODAL -->
+	<div id="loginModal" class="modal fade" role="dialog">
+		<div class="modal-dialog">
+			<!-- Modal content-->
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal">&times;</button>
+				</div>
+				<div class="modal-body text-center">
+					<div class="well">
+						<div class="row">
+							<div class="col-md-6 col-md-offset-3">
+								<h1>LOGIN</h1>
+								<form ng-submit="login(field)">
+									<label for="email">Email</label>
+									<input type="text" class="form-control" id="user" placeholder="Usuário" required ng-model="field.email">
+									<label for="email">Senha</label>
+									<input type="password" class="form-control" id="password" placeholder="Senha" required ng-model="field.password">
+									<br>
+									<button class="btn btn-primary" type="submit">LOGIN</button>
+								</form>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-default" data-dismiss="modal">Fechar</button>
+				</div>
+			</div>
+		</div>
+	</div> <!-- /#modal -->
+
+</div> <!-- .HeaderController -->
