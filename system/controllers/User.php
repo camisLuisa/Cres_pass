0bee667a12a6189af043248df87c614542ebcffe
La Treta <yves_henry13@hotmail.com>
2017-03-14 20:53:06 -0300

    correcao da library, metodo testes no user, esperando novos campos e att do banco de dados

--- system/controllers/User.php
+++ system/controllers/User.php
@@ -21,9 +21,13 @@ class User extends Controller
 	public function login(){
 		// pegar os dados do front end
 		$data = $this->get_post();
-		//logica base
-		//validar username
-		//validateUsername($data['username']);
+
+		if(!$this->lib['Validation']->validateUsername($data['username'])){
+			$this->return['success'] = FALSE;
+			$this->return['error'] .= "Username invalid";
+			return;
+		}
+
 		//procura o usuario
 		$this->model['User_model']->select('user',"WHERE username = '".$data['username']."'");
 		//se achou, login, caso n
@@ -32,56 +36,90 @@ class User extends Controller
 		}else{
 			//mensagem de erro
 			$this->return['success'] = FALSE;
-			$this->return['error'] .= "Login inválido.";
+			$this->return['error'] .= "Não foi encontrado nenhum usuario.";
 		}
 	}
+
+	public function testes(){
+		$testenome = $this->lib['Validation']->validateName('yvesgregorio');
+		$testeemail = $this->lib['Validation']->validateEmail('y_y_@gmail.com');
+		$testeusername = $this->lib['Validation']->validateUsername('y***ves');
+		$testecep = $this->lib['Validation']->validateCEP('11111111');
+		echo "<br>Testando nome<br>";
+		if($testenome){ // Caso não tenha e esteja okay
+			echo "Tudo certo!";
+		}else{ // Caso tenha
+			echo "Contém caracteres inválidos!";
+		}
+		echo "<br>Testando Email<br>";
+		if($testeemail){ // Caso não tenha e esteja okay
+			echo "Tudo certo!";
+		}else{ // Caso tenha
+			echo "Contém caracteres inválidos!";
+		}
+		echo "<br>Testando username<br>";
+		if($testeusername){ // Caso não tenha e esteja okay
+			echo "Tudo certo!";
+		}else{ // Caso tenha
+			echo "Contém caracteres inválidos!";
+		}
+		echo "<br>Testando CEP<br>";
+		if($testecep){ // Caso não tenha e esteja okay
+			echo "Tudo certo!";
+		}else{ // Caso tenha
+			echo "Formato inválido!";
+		}
+	}
+
 
 	public function signup(){
 		// front -> back
 		$data = $this->get_post();
 
 
-		if($this->lib['Validation']->validateEmail($data['email']) != 1){ // Valida email
+		if(!$this->lib['Validation']->validateEmail($data['email'])){ // Valida email
 			$this->return['success'] = FALSE;
 			$this->return['error'] .= "Email inválido. ";
 			
 		}
-		if($this->lib['Validation']->validateUsername($data['username'] != 1)){ //Valida user
+		if(!$this->lib['Validation']->validateUsername($data['username'])){ //Valida user
 			$this->return['success'] = FALSE;
 			$this->return['error'] .= "Nome de user inválido. ";
 		}
 
-		if($this->return['success'] == TRUE){
 
-			
-			//Consultar banco de dados
-			$this->model['User_model']->select('user', "WHERE username = '". $data['username'] ."'");
-			
-			//Verificar TRUE ou FALSE da Consulta
-			if($this->model['User_model']->get_result()){
 
-				//CASO: Já Existe!
-				$this->return['success'] = FALSE;
-				$this->return['error'] .= "Não foi possível cadastrar o user";
 
-			}else{
-				
-				//CASO: Não existe!
-				unset($data['passwordcheck']); // Campo não utilizado na hora de inserção
-				$this->model['User_model']->insert('user', $data);
-				if($this->model['User_model']->get_result()){
-					//CASO: Inserção concluída
-				}
-				else{
-					//CASO: Erro na inserção
-					$this->return['success'] = FALSE;
-					$this->return['error'] .= "Não foi possível cadastrar o user";
-				}
-			}		
 
+		if($this->return['success'] == FALSE){
+			echo "Saindo da insercao.   ";
+			return;
 		}
 
+		//Consultar banco de dados
+		$this->model['User_model']->select('user', "WHERE username = '". $data['username'] ."'");
 			
+		//Verificar TRUE ou FALSE da Consulta
+		if($this->model['User_model']->get_result()){
+
+			//CASO: Já Existe!
+			$this->return['success'] = FALSE;
+			$this->return['error'] .= "Não foi possível cadastrar o user";
+
+		}else{
+				
+				//CASO: Não existe!
+			unset($data['passwordcheck']); // Campo não utilizado na hora de inserção
+			$this->model['User_model']->insert('user', $data);
+			if($this->model['User_model']->get_result()){
+					//CASO: Inserção concluída
+			}
+			else{
+				//CASO: Erro na inserção
+				$this->return['success'] = FALSE;
+				$this->return['error'] .= "Não foi possível cadastrar o user";
+			}
+		}
 	}
 
 }
--- system/libraries/validation.php
+++ system/libraries/validation.php
@@ -20,40 +20,24 @@ class Validation extends Lib
         $validacao = $conta.$provedor.$extension;
         //Retorno da Validação		
         return preg_match($validacao, $email);
-  }
-
-
-  	function validateUsername($user){
-
-      return preg_match("^[a-zA-Z0-9_.]$^", $user);
-
   	}
-  	function validateName($name){
 
-  	}
-  	function validateCEP($cep){
+  	public function validateUsername($user){
+    	return !preg_match("/[*]/", $user);
+ 	}
 
+ 	public function validateName($name){
+    	//    Explaining
+    	//padrao contém a seguinte expressão, caso algo não pertencente ao grupo
+    	//a - z será verdadeiro, logo quando for verdadeiro não deverá ser validado, retornando o 
+    	//inverso.
+ 		$padrao = "/[^a-z]/";
+    	return !preg_match($padrao, $name);
+
+ 	}
+
+ 	public function validateCEP($cep){
+      return preg_match("/[0-9]{5}\-[0-9]{3}/", $cep);
   		//^\d{5}\-\d{3}$ formato de CEP
-
-  	}
+ 	}
 }
-
-
-/* Expressões regulares comuns
-
-	// --------------- Função de testes  --------------------- //
-
-	ereg(String de formato, variavel do campo); TRUE se dar match, FALSE caso n
-
-	// ------------------------------------------------------- //
-
-	^[0-9][a-z]$ = Valida qualquer string q comece com um número e termine com uma letra lowcase
-	^[^0-9][A-Z]$ = valida qualquer string que Não começa com um número e tem letras em Caps
-	^z{1,3}$ = valida z, zz , zzz
-	^z{3,5}$ = valida zzz,zzzz e zzzzz
-	^\d{5}\-\d{3}$ = valida o cep brasileiro
-
-	^([A-Za-z0-9_]{1,}) = Validação Pré @ - Aceita "_" e "."
-	^[a-z]([a-z0-9_.]{1,})\@([a-z]{1,}\.)([com]{3})(\.([br]{2}))?$ - Quase perfeito Verificao de email
-
-*/
