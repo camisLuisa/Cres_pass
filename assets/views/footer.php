05b0e32ae781acbfa22abea61ce4dfff8ea53cbb
camisLuisa <clfl@ecomp.poli.br>
2017-03-15 16:14:59 -0300

    header and footer

--- assets/views/footer.php
+++ assets/views/footer.php
@@ -20,7 +20,7 @@
 			<div class="row">
 				<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
 					<p>Formas de pagamento</p>
-						<img src="https://4.bp.blogspot.com/-j3WYgyvIHFQ/Vuy8fF9_O_I/AAAAAAAABgg/nBWt4XE2N_Y32tvVvhGPlIjtaW3ecRV6g/s1600/Bandeiras%2Baceitas%2Bpela%2BPayleven.jpg" width="500" height="100">
+						<img src="http://www.ficoupequeno.com/images/bandeiras-cartoes-footer.png" width="500" height="100">
 				</div>
 			</div>
 		</div>
