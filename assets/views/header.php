8ce76ac9cf5d29b19b8fd3d2df9019a6e3e26229
camisLuisa <clfl@ecomp.poli.br>
2017-03-15 15:12:09 -0300

    header and general

--- assets/views/header.php
+++ assets/views/header.php
@@ -1,57 +1,51 @@
 
 <div ng-controller="HeaderController">
-	<div class="container-fluid">	
-		<div class="row">	
-			<nav class="navbar navbar-default"><!-- queria por o navbar fixed mas aconteceu um problema semelhante a algo que alyson uma vez falou... perguntar a ele como resolver -->
-			
-				<div class="container">
-					<div class="row">
-						<div class="col-md-4">
-							<a href="http://localhost/Cres_pass/" title="crescendo e passando"><img src="http://www.mibebeyyo.com/images/home/seguridad-objetos-cocina-1293.jpg" class="img-circle" width="100" height="100"></a>
-						</div>
-						
-						<div class="col-md-4">
-						 	<form class="navbar-form" ng-submit="search(searchInput)">
-							 		<div class="form-group">
-							 			<input ng-model="searchInput" type="text class="form-control" placeholder="Procure">
-							 		</div>
-							 		<button type="submit">Buscar</button>
-							</form>
-						 </div>
-
-						 <div class="col-md-4">
-						 		<button ng-click="go('/login')">LOGIN</button>
-
-						 		<button ng-click="go('/cadastro')">CADASTRO</button>
-						</div> 
-					</div>
-				</div>	
+	
+		<!--talvez por a coluna mas n sei qual o tamanho-->
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-4 col-md-offset-5">
+				<a href="http://localhost/Cres_pass/" title="crescendo e passando"><img src="http://www.mibebeyyo.com/images/home/seguridad-objetos-cocina-1293.jpg" class="img-circle" width="100" height="100"></a>
+			</div>
+		</div>
+	</div>		
 					
-				<div class="container">
-					<div class="row">
-					<div class="col-md-12">
-							<ul class="nav navbar-nav">
-								<li class="dropdown"><a class="dropbtn" data-toggle="dropdown" href="#">Produtos<span class="caret"></span></a>
-							        <ul class="dropdown-content">
-							          <li><a href="#">Page 1-1</a></li>
-							          <li><a href="#">Page 1-2</a></li>
-							          <li><a href="#">Page 1-3</a></li>
-							        </ul>
-							      </li>
-								<li class="dropdown"><a class="dropbtn" data-toggle="dropdown" href="#">Marcas<span class="caret"></span></a>
-							        <ul class="dropdown-content">
-							          <li><a href="#">Page 1-1</a></li>
-							          <li><a href="#">Page 1-2</a></li>
-							          <li><a href="#">Page 1-3</a></li>
-							        </ul>
-							      </li>
-								<li><a href="#">Lojinhas</a></li>
-							</ul>
-						</div>
-					</div>
-				</div>
+	<div class="container-fluid">
+		<div class="row">	
+			<div class="col-md-12">
+				<nav class="navbar navbar-inverse" data-spy="affix" data-offset-top="197">
+					<ul class="nav navbar-nav">
+						<li class="dropdown"><a class="dropbtn" data-toggle="dropdown" href="#">Produtos<span class="caret"></span></a>
+					        <ul class="dropdown-content">
+					          <li><a href="#">Page 1-1</a></li>
+					          <li><a href="#">Page 1-2</a></li>
+					          <li><a href="#">Page 1-3</a></li>
+					        </ul>
+					      </li>
+						<li class="dropdown"><a class="dropbtn" data-toggle="dropdown" href="#">Marcas<span class="caret"></span></a>
+					        <ul class="dropdown-content">
+					          <li><a href="#">Page 1-1</a></li>
+					          <li><a href="#">Page 1-2</a></li>
+					          <li><a href="#">Page 1-3</a></li>
+					        </ul>
+					      </li>
+						<li><a href="#">Lojinhas</a></li>
+						<li><a ng-click="go('/login')" class="btn btn-alert">login</a></li>
+						<li><a ng-click="go('/cadastro')">cadastro</a></li>
+					</ul>
+					<form class="navbar-form" ng-submit="search(searchInput)">
+				 		<div class="form-group">
+				 			<input ng-model="searchInput" type="text class="form-control" placeholder="Procure">
+				 		</div>
+				 		<button type="submit">Buscar</button>
+					</form>
 
-			</nav>
+
+				</nav>
+			</div>
 		</div>
 	</div>
+	
+		
+	
 </div>
