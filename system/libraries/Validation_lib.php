71d3a9bf62054ba69f0bcdedded071a0a585b2b8
La Treta <yves_henry13@hotmail.com>
2017-03-16 15:38:34 -0300

    documentação EN

--- system/libraries/Validation_lib.php
+++ system/libraries/Validation_lib.php
@@ -9,10 +9,11 @@ class Validation_lib extends Lib
 	}
 
 	public function callForValidation($data){
-		// Criacao de array semelhante ao return do controller para compatibilidade e retorno dele pronto
+		// Creates a array like $this->return from controller to return it with compatibility
 		$return = array('success' => TRUE, 'error' => "");
-		// Processo simples, se houver um campo com chave e conteudo definidos
-		// chama sua respectiva validacao e agrega mensagem e status
+		//Simple process, it verifies if the array $data contains a defined key
+		// if it's true, it validates his content and adds any error messages that it needs
+		// then return the array to the controller used to show the error messages
 		if(isset($data['name'])){
 			$status = $this->validateName($data['name']);
 			if(!$status){
@@ -110,60 +111,86 @@ class Validation_lib extends Lib
 
 	public function validateEmail($email){
 
-        // De a-z,0-9, ponto e underscore(underline) com @
+				// From a to z, 0 to 9, dot and underscore with  @ at the end
         $conta = "^([a-z]|[\._-])+[@]";
-        // Provedor do email apenas letras
+        // Email provider, a to z with a dot at the end
         $provedor = "[a-z]+[.]";
         // com.br e com
-				// Caso exista br no final, deverá vir acompanhado de um ponto, o ? demarca oq vem antes é opcional
+				// if do exist a "br" at the final, it should come with a dot before it, the ? informs that ".br" is optional
+				// the ? means the expression before, in this case .br is optional, not the entire regular expression
         $extension = "[com]{3}([\.][br]{2})?$^";
-        //Concatenação de todas as expressões regulares separadas
+        //Concatenation of the expressions for modularity purposes
         $validacao = $conta.$provedor.$extension;
-        //Retorno da Validação
+        // returning the TRUE or FALSE statement from the validation process
         return preg_match($validacao, $email);
   }
 
  	public function validateName($name){
+		// \A Begin of the string flag
+		// \z End of the string flag
+		// [a-z] a to z group
+		// + * or {0,} quantifiers to set the number of digits i would want
+		// since only letters should be allowed, a to z and + after
  		$padrao = "/\A[a-z]+\z/";
     return preg_match($padrao, $name);
  	}
 
  	public function validateCEP($cep){
-      //Se não obedecer o formato 5 números hífen e 3 números, ele torna inválido!
-			$padrao = "/[0-9]{5}\-?[0-9]{3}/";
+			// Most common known as ZIP CODE, We call it cep
+			// it's format is 12345-678
+			// So \A to begin string
+			//[0-9] 0 to 9
+			// {5} five times ([0-9]) meaning 5 numbers in sequence
+			// - ? means that the user don't have to type the "-", but yet it accepts
+			// then another 0 to 9 3 times ([0-9]{3})
+			// then \z to end the string
+			$padrao = "/\A[0-9]{5}\-?[0-9]{3}\z/";
       return preg_match($padrao, $cep);
  	}
 
 	public function validateUsername($user){
-			$padrao = "/\A[a-z]+([a-z\_\.]{1,}?)\z/";
+			//Username should only contain letters, numbers, underscores and dots
+			// So needs to start with a to z
+			// then can have as much 0 to 9 or a to z, underscore and dots as it wants
+			// Needs at least 4 letters
+			$padrao = "/\A[a-z]{4}+([a-z0-9\_\.]{1,}?)\z/";
     	return preg_match($padrao, $user);
  	}
 
 	public function validateCPF($cpf){
+		// Format xxx xxx xxx xx
+		// 11 numbers without space
 		$padrao = "/\A\d{11}\z/";
 		return preg_match($padrao, $cpf);;
 
 	}
 
 	public function validatePhone($telefone){
+		// Allows 8 or 9 digits phone numbers
 		return preg_match("/\A[0-9]{8,9}$\z/", $telefone);
 	}
 
 	public function validateDDD($ddd){
+		//allows only ddd with 2 digits
 		$padrao = "/\A[0-9]{2}\z/";
 		return preg_match($padrao, $ddd);
 	}
 
 	public function validateAddress($adress){
+		// Allows any combination of letters of any lenght
 		$padrao = "/\A[A-z\ ]*\z/";
 		return preg_match($padrao, $adress);
 	}
 
 	public function validateNumber($number){
-		return preg_match("/\A\d{0,6}\z/", $number);
+		// allows address numbers with 1 to 6 digits
+		return preg_match("/\A\d{1,6}\z/", $number);
 	}
 
 	public function validateReference($ref){
+		// Allows numbers and letters, spaces of any size
+		// Since it contains the same conditions of complement field
+		// validates both of them with the same method
 		$padrao = "/\A[A-z0-9 ]*\z/";
 		return preg_match($padrao, $ref);
 	}
