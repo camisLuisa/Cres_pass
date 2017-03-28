802aa959ad5c02289769bf847ade296c6f04d3da
FederalCircle <alyson.maia@gmail.com>
2017-03-27 22:27:21 -0300

    clean up

--- assets/js/controllers/home-controller.js
+++ assets/js/controllers/home-controller.js
@@ -1,18 +1,6 @@
 app.controller('HomeController', function($scope, $location, $routeParams, $http){
 	$scope.home = "HOME";
-
-	$scope.return;
-
 	$scope.go = function(path) {
 		$location.path(path);
 	}
-
-	$scope.link = function(){
-		$location.path("/page/50");
-	}
-
-	$http.post('system/test/square', {num: 10})
-	.then(function(response){
-		$scope.return = response.data;
-	});
 });
