7701f796641ec8ef896acdce457c869268795475
camisLuisa <clfl@ecomp.poli.br>
2017-03-28 15:23:00 -0300

    signup post

--- assets/js/controllers/signup-controller.js
+++ assets/js/controllers/signup-controller.js
@@ -10,6 +10,7 @@ app.controller('SignUpController', function($scope, $location, $routeParams, $ht
 
 		$http.post('system/user/signup', field)
 		.then(function(response) {
+			
 			if (response.data.success) {
 				console.log('Usuário cadastrado.');
 			}
