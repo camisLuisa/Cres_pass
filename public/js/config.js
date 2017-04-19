583594d5475d3c4852e4540218a94436b1b8a808
Alyson Maia <alyson.maia@gmail.com>
2017-04-18 23:20:19 -0300

    Provider add

--- public/js/config.js
+++ public/js/config.js
@@ -4,36 +4,56 @@
  */
 app.config(function($stateProvider, $locationProvider, $urlRouterProvider){
  	$locationProvider.html5Mode(true);
+ 	
+	/*Constant paths to public sub-folders*/
+ 	$pathToProvider
+		.addPath({
+			name:"jsFolder",
+			folder:"js"
+		})
+		.addPath({
+			name:"directivesFolder",
+			folder:"directives",
+			parent:"jsFolder"
+		})
+		.addPath({
+			name:"viewsFolder",
+			folder:"views"
+		});
 
+	/*Getting the paths defined by pathToProvider*/
+	var pathTo = $pathToProvider.$get();
+
+	/*UI-Router states*/
 	$stateProvider
 	.state({
 		name: 'base',
-		templateUrl: viewsPath+'base.php',
+		templateUrl: pathTo.viewsFolder+'base.php',
 		controller: 'BaseController'
 	})
 	.state({
 		name: 'base.home',
 		url: '/',
-		templateUrl: viewsPath+'home.php',
+		templateUrl: pathTo.viewsFolder+'home.php',
 		controller: 'HomeController'
 	})
 	.state({
 		name: 'base.signup',
 		url: '/cadastro',
-		templateUrl: viewsPath+'signup.php',
+		templateUrl: pathTo.viewsFolder+'signup.php',
 		controller: 'SignUpController'
 	})
 	.state({
 		name: 'base.panel',
 		url: '/painel',
-		templateUrl: viewsPath+'panel.php',
+		templateUrl: pathTo.viewsFolder+'panel.php',
 		controller: 'PanelController'
 	})
 
 	.state({
 		name: 'base.panel.signup',
 		url: '/signupEdit',
-		templateUrl: viewsPath+'signupEdit.php',
+		templateUrl: pathTo.viewsFolder+'signupEdit.php',
 		controller: 'SignUpEditController'
 	})
 	;
